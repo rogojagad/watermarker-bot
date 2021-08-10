b35633467d66de919adf49f42b07de08a16902ad
rogojagad <rogojagadalit@gmail.com>
2021-08-10 17:56:17 +0700

    setup dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+  ],
+  rules: {
+    'no-console': 'warn',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+  },
+};
